ad0b7e435fe67495ca39a86d14cf039e2f09a33c
pajones96 <pajonesvpbe@gmail.com>
2022-05-11 22:20:46 -0400

    Some basic setup done

---
+++ dogOdyssey/src/main/java/com/augur/dogOdyssey/DogOdysseyApplication.java
@@ -0,0 +1,13 @@
+package com.augur.dogOdyssey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogOdysseyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogOdysseyApplication.class, args);
+	}
+
+}
