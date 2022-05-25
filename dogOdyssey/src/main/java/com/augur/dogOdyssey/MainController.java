46ba242ba961f243bbfebf97dd7e84ad18706190
pajones96 <pajonesvpbe@gmail.com>
2022-05-24 23:58:45 -0400

    Little tweak, placeholder in maincontroller while I read docs

--- dogOdyssey/src/main/java/com/augur/dogOdyssey/MainController.java
+++ dogOdyssey/src/main/java/com/augur/dogOdyssey/MainController.java
@@ -1,8 +1,8 @@
 package com.augur.dogOdyssey;
 
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcProperties;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.function.ServerResponse;
 
 @RestController
@@ -14,8 +14,11 @@ public class MainController {
         return "Full Stack Java with Spring Boot & VueJS!";
     }
 
-    public ServerResponse testThingy(){
-        return ServerResponse.ok().build();
+    //Looking at some documentation for a bit, this is a placeholder
+    @PostMapping
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void sendMessage(){
+
     }
 
 
