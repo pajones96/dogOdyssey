7c63f3c458f35f1f7fefefc894bb63216b605a70
pajones96 <pajonesvpbe@gmail.com>
2022-05-31 23:38:41 -0400

    quick work to prep for integrating mongodb

---
+++ dogOdyssey/src/backend/java/com/augur/dogOdyssey/UserRepository.java
@@ -0,0 +1,10 @@
+package com.augur.dogOdyssey;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional(readOnly = true)
+public interface UserRepository {
+
+}
