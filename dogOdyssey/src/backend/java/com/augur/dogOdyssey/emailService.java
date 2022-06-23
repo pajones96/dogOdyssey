1ba6461c38b5ecba97e4527a5bcea75b902e1861
pajones96 <pajonesvpbe@gmail.com>
2022-06-22 23:57:13 -0400

    Connecting basic bits and pieces

--- dogOdyssey/src/backend/java/com/augur/dogOdyssey/emailService.java
+++ dogOdyssey/src/backend/java/com/augur/dogOdyssey/emailService.java
@@ -1,15 +1,20 @@
 package com.augur.dogOdyssey;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class emailService {
+
     @Autowired
     private JavaMailSender emailSender;
 
     public void sendEmail(contactForm cForm){
-
+        SimpleMailMessage userMessage = new SimpleMailMessage();
+        userMessage.setFrom(cForm.email);
+        userMessage.setSubject("New message from" + cForm.email);
+        userMessage.setText(cForm.messageBody);
     }
 }
